21158f6d33d60c2c305201ef09f6b5d7abc1ea8d
RenaldMorino <renaudmorin2001@gmail.com>
2025-01-03 15:58:11 -0500

    Added sound effect on hover of boo text
    - Added a wrapper componenent that loads all the sounds and randomizes
      the one that plays and it's volume.

--- src/App.jsx
+++ src/App.jsx
@@ -1,133 +1,140 @@
 // Packages
-import { createTheme, TableCell, ThemeProvider, Table, TableRow } from '@mui/material'
-import makeStyles from '@mui/styles/makeStyles'
-import '@fontsource/sonsie-one'
-import '@fontsource/raleway'
-import '@fontsource/noto-serif'
-import { track } from '@vercel/analytics'
+import {
+  createTheme,
+  TableCell,
+  ThemeProvider,
+  Table,
+  TableRow,
+} from "@mui/material";
+import makeStyles from "@mui/styles/makeStyles";
+import "@fontsource/sonsie-one";
+import "@fontsource/raleway";
+import "@fontsource/noto-serif";
+import { track } from "@vercel/analytics";
 
 // Icons
-import EmailIcon from '@mui/icons-material/Email'
-import GitHubIcon from '@mui/icons-material/GitHub'
-import TwitterIcon from '@mui/icons-material/Twitter'
-import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 // Components
-import Palette from './Components/Palette'
-import Konami from 'react-konami-code'
-import WinSound from './Assets/Sounds/8-bit-victory-sound-101319.mp3'
-import { ReactComponent as EasterEggTopRightIcon } from './Assets/Icons/easter-egg-design.svg'
-import { motion } from 'framer-motion'
-import KonamiModal from './Components/KonamiModal'
+import Palette from "./Components/Palette";
+import Konami from "react-konami-code";
+import WinSound from "./Assets/Sounds/8-bit-victory-sound-101319.mp3";
+import { ReactComponent as EasterEggTopRightIcon } from "./Assets/Icons/easter-egg-design.svg";
+import { motion } from "framer-motion";
+import KonamiModal from "./Components/KonamiModal";
 
 // Hooks
-import useSound from 'use-sound'
-import { useState } from 'react'
+import useSound from "use-sound";
+import { useState } from "react";
+import BooSoundSystem from "./Components/BooSoundSystem";
 
-const theme = createTheme({})
+const theme = createTheme({});
 const useStyles = makeStyles((theme) => ({
   mainContainer: {
     background: Palette.main.darkBackground,
     color: Palette.main.lightYellow,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    height: '100%',
-    outline: '10px solid',
-    outlineOffset: '-10px',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    height: "100%",
+    outline: "10px solid",
+    outlineOffset: "-10px",
     outlineColor: Palette.main.lightYellow,
 
-    '-webkit-touch-callout': 'none' /* iOS Safari */,
-    '-webkit-user-select': 'none' /* Safari */,
-    '-khtml-user-select': 'none' /* Konqueror HTML */,
-    '-moz-user-select': 'none' /* Old versions of Firefox */,
-    '-ms-user-select': 'none' /* Internet Explorer/Edge */,
-    userSelect: 'none' /* Non-prefixed version, currently */
+    "-webkit-touch-callout": "none" /* iOS Safari */,
+    "-webkit-user-select": "none" /* Safari */,
+    "-khtml-user-select": "none" /* Konqueror HTML */,
+    "-moz-user-select": "none" /* Old versions of Firefox */,
+    "-ms-user-select": "none" /* Internet Explorer/Edge */,
+    userSelect: "none" /* Non-prefixed version, currently */,
   },
 
   mainTextContainer: {
-    fontFamily: 'Sonsie One',
-    fontSize: 'calc(50px + 5vw);',
-    paddingTop: 'calc(50px + 2vw);'
+    fontFamily: "Sonsie One",
+    fontSize: "calc(50px + 5vw);",
+    paddingTop: "calc(50px + 2vw);",
   },
 
   subtitleTextContainer: {
-    fontFamily: 'Raleway',
-    fontSize: 'calc(20px + 0.5vw);',
-    paddingTop: 'calc(20px + 2vw);',
-    fontWeight: 'bold'
+    fontFamily: "Raleway",
+    fontSize: "calc(20px + 0.5vw);",
+    paddingTop: "calc(20px + 2vw);",
+    fontWeight: "bold",
   },
 
   ipaTextContainer: {
-    fontFamily: 'Noto Serif',
-    fontSize: 'calc(10px + 1vw);',
-    paddingTop: 'calc(5px + 0.25vw);',
-    fontWeight: 'bold'
+    fontFamily: "Noto Serif",
+    fontSize: "calc(10px + 1vw);",
+    paddingTop: "calc(5px + 0.25vw);",
+    fontWeight: "bold",
   },
 
   contactMeContainer: {
-    paddingTop: 'calc(15vh - 0.25vw);'
+    paddingTop: "calc(15vh - 0.25vw);",
   },
 
   iconColor: {
-    color: Palette.main.lightYellow
+    color: Palette.main.lightYellow,
   },
 
   bottomTextContainer: {
-    fontFamily: 'Sonsie One',
-    position: 'absolute',
+    fontFamily: "Sonsie One",
+    position: "absolute",
     color: Palette.main.lightBlue,
-    width: '2rem',
-    right: '10vw',
-    bottom: '5vh'
+    width: "2rem",
+    right: "10vw",
+    bottom: "5vh",
   },
 
   tableCellCustomColor: {
-    borderColor: Palette.main.flashyPink + ' !important',
-    borderWidth: '1px',
-    '&:hover': {
-      borderWidth: '5px'
-    }
+    borderColor: Palette.main.flashyPink + " !important",
+    borderWidth: "1px",
+    "&:hover": {
+      borderWidth: "5px",
+    },
   },
 
   easterEggTopRightIcon: {
-    position: 'absolute',
-    top: '30px',
-    right: '30px',
-    width: 'auto',
-    height: 'auto',
-    cursor: 'pointer',
-    '& svg': {
+    position: "absolute",
+    top: "30px",
+    right: "30px",
+    width: "auto",
+    height: "auto",
+    cursor: "pointer",
+    "& svg": {
       fill: Palette.main.lightYellow,
-      width: '5rem',
-      height: '5rem'
-    }
+      width: "5rem",
+      height: "5rem",
+    },
   },
 
   easterEggDarkClicked: {
-    '& svg': {
-      fill: Palette.main.darkAccent + ' !important'
-    }
-  }
-}))
+    "& svg": {
+      fill: Palette.main.darkAccent + " !important",
+    },
+  },
+}));
 
 const App = () => {
-  const classes = useStyles()
-  const [playWinSound] = useSound(WinSound, { volume: 0.025 })
-  const [konamiCompleted, setKonamiCompleted] = useState(false)
-  const [eggClicked, setEggClicked] = useState(false)
-  const [konamiModalOpened, setKonamiModalOpened] = useState(false)
+  const classes = useStyles();
+  const [playWinSound] = useSound(WinSound, { volume: 0.025 });
+  const [konamiCompleted, setKonamiCompleted] = useState(false);
+  const [eggClicked, setEggClicked] = useState(false);
+  const [konamiModalOpened, setKonamiModalOpened] = useState(false);
 
   const handleKonami = () => {
-    setKonamiCompleted(true)
-    playWinSound()
-    track('konami-code-completed')
-  }
+    setKonamiCompleted(true);
+    playWinSound();
+    track("konami-code-completed");
+  };
 
   const handleEggClick = () => {
-    setEggClicked(true)
-    setKonamiModalOpened(!konamiModalOpened)
-  }
+    setEggClicked(true);
+    setKonamiModalOpened(!konamiModalOpened);
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -139,21 +146,21 @@ const App = () => {
         {konamiCompleted && (
           <>
             <div
-              className={`${classes.easterEggTopRightIcon} ${eggClicked ? classes.easterEggDarkClicked : ''}`}
+              className={`${classes.easterEggTopRightIcon} ${eggClicked ? classes.easterEggDarkClicked : ""}`}
               onClick={() => handleEggClick()}
             >
               <motion.div
                 animate={{
                   rotate: !eggClicked
                     ? [0, -35, 45, -25, 35, -15, 15, -5, 5, 0]
-                    : [0]
+                    : [0],
                 }}
                 transition={{
                   repeat: Infinity,
                   // TODO - Fix magic numbers
                   repeatDelay: Math.floor(Math.random() * (8 - 2 + 1) + 2),
                   duration: 1,
-                  stiffness: 100
+                  stiffness: 100,
                 }}
               >
                 <EasterEggTopRightIcon />
@@ -172,36 +179,36 @@ const App = () => {
           <Table>
             <TableRow>
               <a
-                href='mailto:renaudmorinwork@gmail.com'
-                target='_blank'
-                rel='noreferrer'
+                href="mailto:renaudmorinwork@gmail.com"
+                target="_blank"
+                rel="noreferrer"
               >
                 <TableCell className={classes.tableCellCustomColor}>
                   <EmailIcon className={classes.iconColor} />
                 </TableCell>
               </a>
               <a
-                href='https://github.com/RenaldMorino'
-                target='_blank'
-                rel='noreferrer'
+                href="https://github.com/RenaldMorino"
+                target="_blank"
+                rel="noreferrer"
               >
                 <TableCell className={classes.tableCellCustomColor}>
                   <GitHubIcon className={classes.iconColor} />
                 </TableCell>
               </a>
               <a
-                href='https://twitter.com/RenaldiniMorino'
-                target='_blank'
-                rel='noreferrer'
+                href="https://twitter.com/RenaldiniMorino"
+                target="_blank"
+                rel="noreferrer"
               >
                 <TableCell className={classes.tableCellCustomColor}>
                   <TwitterIcon className={classes.iconColor} />
                 </TableCell>
               </a>
               <a
-                href='https://www.linkedin.com/in/renaud-morin-b6a9b113a/'
-                target='_blank'
-                rel='noreferrer'
+                href="https://www.linkedin.com/in/renaud-morin-b6a9b113a/"
+                target="_blank"
+                rel="noreferrer"
               >
                 <TableCell className={classes.tableCellCustomColor}>
                   <LinkedInIcon className={classes.iconColor} />
@@ -210,11 +217,13 @@ const App = () => {
             </TableRow>
           </Table>
         </div>
-        <div className={classes.bottomTextContainer}>boo</div>
+        <BooSoundSystem
+          children={<div className={classes.bottomTextContainer}>boo</div>}
+        />
         <Konami action={handleKonami} />
       </div>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Components/BooSoundSystem.jsx
@@ -0,0 +1,188 @@
+// Sound files
+import BooSound1 from '../Assets/Sounds/BooSounds/boo-01.mp3'
+import BooSound2 from '../Assets/Sounds/BooSounds/boo-02.mp3'
+import BooSound3 from '../Assets/Sounds/BooSounds/boo-03.mp3'
+import BooSound4 from '../Assets/Sounds/BooSounds/boo-04.mp3'
+import BooSound5 from '../Assets/Sounds/BooSounds/boo-05.mp3'
+import BooSound6 from '../Assets/Sounds/BooSounds/boo-06.mp3'
+import BooSound7 from '../Assets/Sounds/BooSounds/boo-07.mp3'
+import BooSound8 from '../Assets/Sounds/BooSounds/boo-08.mp3'
+import BooSound9 from '../Assets/Sounds/BooSounds/boo-09.mp3'
+import BooSound10 from '../Assets/Sounds/BooSounds/boo-10.mp3'
+import BooSound11 from '../Assets/Sounds/BooSounds/boo-11.mp3'
+import BooSound12 from '../Assets/Sounds/BooSounds/boo-12.mp3'
+import BooSound13 from '../Assets/Sounds/BooSounds/boo-13.mp3'
+import BooSound14 from '../Assets/Sounds/BooSounds/boo-14.mp3'
+import BooSound15 from '../Assets/Sounds/BooSounds/boo-15.mp3'
+import BooSound16 from '../Assets/Sounds/BooSounds/boo-16.mp3'
+import BooSound17 from '../Assets/Sounds/BooSounds/boo-17.mp3'
+import BooSound18 from '../Assets/Sounds/BooSounds/boo-18.mp3'
+import BooSound19 from '../Assets/Sounds/BooSounds/boo-19.mp3'
+import BooSound20 from '../Assets/Sounds/BooSounds/boo-20.mp3'
+import BooSound21 from '../Assets/Sounds/BooSounds/boo-21.mp3'
+import BooSound22 from '../Assets/Sounds/BooSounds/boo-22.mp3'
+import BooSound23 from '../Assets/Sounds/BooSounds/boo-23.mp3'
+import BooSound24 from '../Assets/Sounds/BooSounds/boo-24.mp3'
+import BooSound25 from '../Assets/Sounds/BooSounds/boo-25.mp3'
+import BooSound26 from '../Assets/Sounds/BooSounds/boo-26.mp3'
+
+import useSound from 'use-sound'
+import { useState } from 'react'
+
+const BooSoundSystem = ({ children }) => {
+  const maxVolume = 0.55
+  const minVolume = 0.45
+
+  const players = []
+  const stoppers = []
+  const [playBoo1, { stop: stopBoo1 }] = useSound(BooSound1, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo2, { stop: stopBoo2 }] = useSound(BooSound2, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo3, { stop: stopBoo3 }] = useSound(BooSound3, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo4, { stop: stopBoo4 }] = useSound(BooSound4, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo5, { stop: stopBoo5 }] = useSound(BooSound5, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo6, { stop: stopBoo6 }] = useSound(BooSound6, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo7, { stop: stopBoo7 }] = useSound(BooSound7, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo8, { stop: stopBoo8 }] = useSound(BooSound8, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo9, { stop: stopBoo9 }] = useSound(BooSound9, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo10, { stop: stopBoo10 }] = useSound(BooSound10, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo11, { stop: stopBoo11 }] = useSound(BooSound11, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo12, { stop: stopBoo12 }] = useSound(BooSound12, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo13, { stop: stopBoo13 }] = useSound(BooSound13, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo14, { stop: stopBoo14 }] = useSound(BooSound14, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo15, { stop: stopBoo15 }] = useSound(BooSound15, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo16, { stop: stopBoo16 }] = useSound(BooSound16, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo17, { stop: stopBoo17 }] = useSound(BooSound17, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo18, { stop: stopBoo18 }] = useSound(BooSound18, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo19, { stop: stopBoo19 }] = useSound(BooSound19, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo20, { stop: stopBoo20 }] = useSound(BooSound20, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo21, { stop: stopBoo21 }] = useSound(BooSound21, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo22, { stop: stopBoo22 }] = useSound(BooSound22, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo23, { stop: stopBoo23 }] = useSound(BooSound23, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo24, { stop: stopBoo24 }] = useSound(BooSound24, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo25, { stop: stopBoo25 }] = useSound(BooSound25, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+  const [playBoo26, { stop: stopBoo26 }] = useSound(BooSound26, {
+    volume: Math.random() * (maxVolume - minVolume) + minVolume
+  })
+
+  players.push(playBoo1)
+  players.push(playBoo2)
+  players.push(playBoo3)
+  players.push(playBoo4)
+  players.push(playBoo5)
+  players.push(playBoo6)
+  players.push(playBoo7)
+  players.push(playBoo8)
+  players.push(playBoo9)
+  players.push(playBoo10)
+  players.push(playBoo11)
+  players.push(playBoo12)
+  players.push(playBoo13)
+  players.push(playBoo14)
+  players.push(playBoo15)
+  players.push(playBoo16)
+  players.push(playBoo17)
+  players.push(playBoo18)
+  players.push(playBoo19)
+  players.push(playBoo20)
+  players.push(playBoo21)
+  players.push(playBoo22)
+  players.push(playBoo23)
+  players.push(playBoo24)
+  players.push(playBoo25)
+  players.push(playBoo26)
+
+  stoppers.push(stopBoo1)
+  stoppers.push(stopBoo2)
+  stoppers.push(stopBoo3)
+  stoppers.push(stopBoo4)
+  stoppers.push(stopBoo5)
+  stoppers.push(stopBoo6)
+  stoppers.push(stopBoo7)
+  stoppers.push(stopBoo8)
+  stoppers.push(stopBoo9)
+  stoppers.push(stopBoo10)
+  stoppers.push(stopBoo11)
+  stoppers.push(stopBoo12)
+  stoppers.push(stopBoo13)
+  stoppers.push(stopBoo14)
+  stoppers.push(stopBoo15)
+  stoppers.push(stopBoo16)
+  stoppers.push(stopBoo17)
+  stoppers.push(stopBoo18)
+  stoppers.push(stopBoo19)
+  stoppers.push(stopBoo20)
+  stoppers.push(stopBoo21)
+  stoppers.push(stopBoo22)
+  stoppers.push(stopBoo23)
+  stoppers.push(stopBoo24)
+  stoppers.push(stopBoo25)
+  stoppers.push(stopBoo26)
+
+  const [previousSoundIndex, setPreviousSoundIndex] = useState(null)
+
+  const handlePlay = () => {
+    if (previousSoundIndex !== null) {
+      const stopPreviousSound = stoppers[previousSoundIndex]
+      stopPreviousSound()
+    }
+
+    const nextSoundIndex = Math.floor(Math.random() * players.length)
+    const playNextSound = players[nextSoundIndex]
+    playNextSound()
+    setPreviousSoundIndex(nextSoundIndex)
+  }
+
+  return <div onMouseOver={() => handlePlay()}>{children}</div>
+}
+
+export default BooSoundSystem
